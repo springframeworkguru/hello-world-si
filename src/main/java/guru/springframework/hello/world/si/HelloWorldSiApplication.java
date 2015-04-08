4be77162310ca85e72ec75e03709a4fbfa080f3f
John Thompson <john@springframework.guru>
2015-04-08 07:45:23 -0400

    Initial project add

---
+++ src/main/java/guru/springframework/hello/world/si/HelloWorldSiApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.hello.world.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldSiApplication.class, args);
+    }
+}
